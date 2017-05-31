20618f834e2aca7efdd08ca0b6baa3a7e68ee359
nindyasaridu <nindyasari13@mhs.if.its.ac.id>
2017-05-31 15:18:44 +0700

    save dulu

--- XXTEA/xxtea/tes2.js
+++ XXTEA/xxtea/tes2.js
@@ -1,12 +1,23 @@
+var now = require("performance-now");
 /*jshint node:true, eqeqeq:true */
 'use strict';
 
 var xxtea        = require("./xxtea.js");
 
-var str = "Hello World!";
-var key = "1234567890";
+var str = "ASJDKJSDKJASKJDKSAJDKJASKJDKSAJDWHEQWPEOIREWYUMDNFB";
+var key = "KUNCIGILA";
+
+var t0 = now();
 var encrypt_data = xxtea.encryptToString(str, key);
+var t1 = now();
 console.log(encrypt_data);
+console.log("Call to encrypt took " + (t1 - t0) + " milliseconds.");
+
+var t2 = now();
 var decrypt_data = xxtea.decryptToString(encrypt_data, key);
+var t3 = now();
 console.assert(str === decrypt_data);
 console.log(decrypt_data);
+console.log("Call to decrypt took " + (t3 - t2) + " milliseconds.");
+
+
